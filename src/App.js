5db5f53bed47761206f6ebd96d077601415bc18b
Clayton Ingalls <clayton.ingalls@withonevision.com>
2021-08-30 08:30:51 -0400

    feat: updates to actual songs and adds css variables

--- src/App.js
+++ src/App.js
@@ -9,24 +9,52 @@ export default function App() {
 
   const songs = [
     {
-      title: "some title 1",
-      src: "https://www.dropbox.com/s/i7jc9ojpnsyii2l/Urban%20Sunrise%20.mp3?raw=1&t=.wav&_=1",
+      title: "Now and Then",
+      src: "https://www.dropbox.com/s/jrq05ynz1rex115/01%20Now%20and%20Then.mp3?raw=1",
     },
     {
-      title: "another title",
-      src: "https://www.dropbox.com/s/c26fdsksh9q0nzg/Images%20-%20Lost%20European.wav?raw=1&t=.wav&_=2",
+      title: "Laying Stones",
+      src: "https://www.dropbox.com/s/yp745tkh7g5ssvm/02%20Laying%20Stones.mp3?raw=1",
     },
     {
-      title: "a third title",
-      src: "https://www.dropbox.com/s/36gyipvdty81o51/What%20a%20beautiful%20Sunset%21%20%28radio%20mix%29%20-%20Angelwing.wav?raw=1&t=.wav&_=3",
+      title: "Peace that Passes",
+      src: "https://www.dropbox.com/s/zwl79r2cdhlk5fo/03%20Peace%20that%20Passes.mp3?raw=1",
     },
     {
-      title: "a fourth title",
-      src: "https://www.dropbox.com/s/36gyipvdty81o51/What%20a%20beautiful%20Sunset%21%20%28radio%20mix%29%20-%20Angelwing.wav?raw=1&t=.wav&_=3",
+      title: "Man of Dust",
+      src: "https://www.dropbox.com/s/dwkn9bgl6fkssc1/05%20Man%20of%20Dust.mp3?raw=1",
     },
     {
-      title: "a fifth title",
-      src: "https://www.dropbox.com/s/36gyipvdty81o51/What%20a%20beautiful%20Sunset%21%20%28radio%20mix%29%20-%20Angelwing.wav?raw=1&t=.wav&_=3",
+      title: "Sacred Head",
+      src: "https://www.dropbox.com/s/bxjr8yul46mh3gf/05%20Sacred%20Head.mp3?raw=1",
+    },
+    {
+      title: "We Will Rise",
+      src: "https://www.dropbox.com/s/vbb3n7j3lov80mu/09%20We%20Will%20Rise.mp3?raw=1",
+    },
+    {
+      title: "Wisdom",
+      src: "https://www.dropbox.com/s/z35fr4s2zg14kq8/10%20Wisdom.mp3?raw=1",
+    },
+    {
+      title: "What I've Been Looking For",
+      src: "https://www.dropbox.com/s/cguyfzlvi2z0o7b/11%20What%20I%27ve%20Been%20Looking%20For.mp3?raw=1",
+    },
+    {
+      title: "Lullaby",
+      src: "https://www.dropbox.com/s/2wpqkzvbp2k37xa/12%20Lullaby.mp3?raw=1",
+    },
+    {
+      title: "The Final Time",
+      src: "https://www.dropbox.com/s/cdxp7q0f5a4slgs/12%20The%20Final%20Time.wav?raw=1",
+    },
+    {
+      title: "For All Our Fears",
+      src: "https://www.dropbox.com/s/g5e5687ckf0boq0/For%20all%20our%20Fears.m4a?raw=1",
+    },
+    {
+      title: "Our Eyes are on You",
+      src: "https://www.dropbox.com/s/40tj92n3ayhg56l/Our%20Eyes%20Are%20On%20You.wav?raw=1",
     },
   ];
 
